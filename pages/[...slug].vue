6f4e0262454b826b652c9f1cfbc488fe46d2d626
Dmitry <dima.rolick22@gmail.com>
2023-11-20 12:40:18 +0300

    Added "Slug" page

---
+++ pages/[...slug].vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+definePageMeta({
+  breadcrumb: 'Несуществующая страница',
+})
+
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      <div class="wrapper">
+        <h1 class="title">Cтраница не существует</h1>
+        <div class="media">
+          <img src="/images/slug.png" alt="Empty">
+        </div>
+        <NuxtLink to="/" class="home__btn">На главную</NuxtLink>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped lang="scss">
+.wrapper {
+  width: 100%;
+  padding: 32px 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 42px;
+}
+
+.title {
+  color: $text-dark;
+  font-feature-settings: 'clig' off, 'liga' off;
+  font-family: Roboto;
+  font-size: 48px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+}
+
+.home__btn {
+  padding: 12px 64px;
+  border-radius: 10px;
+  background: $main-color;
+  color: #FFF;
+  font-feature-settings: 'clig' off, 'liga' off;
+  font-family: Roboto;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  transition: all 0.1s ease-in-out;
+
+  &:hover {
+    background-color: $main-color-dark;
+  }
+
+  &:active {
+    scale: 0.95;
+  }
+}
+</style>
