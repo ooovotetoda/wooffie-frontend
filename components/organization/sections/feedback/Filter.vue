dd1cadd59fb58090f67fe77e7de32778fdccd05e
yuga <dima.rolick22@gmail.com>
2024-02-01 01:11:01 +0300

    add: comment counts

--- components/organization/sections/feedback/Filter.vue
+++ components/organization/sections/feedback/Filter.vue
@@ -1,4 +1,16 @@
 <script setup lang="ts">
+const props = defineProps({
+  reviews: Object,
+})
+
+const counts = reactive({
+  all: props.reviews ? props.reviews.length : 0,
+  great: props.reviews ? props.reviews.filter((r: object) => r.rating === 5).length : 0,
+  good: props.reviews ? props.reviews.filter((r: object) => r.rating === 4).length : 0,
+  normal: props.reviews ? props.reviews.filter((r: object) => r.rating === 3).length : 0,
+  bad: props.reviews ? props.reviews.filter((r: object) => r.rating === 2).length : 0,
+  terrible: props.reviews ? props.reviews.filter((r: object) => r.rating === 1).length : 0,
+})
 
 </script>
 
@@ -7,11 +19,11 @@
     <h4 class="filter__title">Фильтр по оценкам</h4>
 
     <ul class="filter__list">
-      <OrganizationSectionsFeedbackLine label="Отлично" :all="16" :count="5"/>
-      <OrganizationSectionsFeedbackLine label="Хорошо" :all="16" :count="6"/>
-      <OrganizationSectionsFeedbackLine label="Нормально" :all="16" :count="1"/>
-      <OrganizationSectionsFeedbackLine label="Плохо" :all="16" :count="3"/>
-      <OrganizationSectionsFeedbackLine label="Ужасно" :all="16" :count="1"/>
+      <OrganizationSectionsFeedbackLine label="Отлично" :all="counts.all" :count="counts.great"/>
+      <OrganizationSectionsFeedbackLine label="Хорошо" :all="counts.all" :count="counts.good"/>
+      <OrganizationSectionsFeedbackLine label="Нормально" :all="counts.all" :count="counts.normal"/>
+      <OrganizationSectionsFeedbackLine label="Плохо" :all="counts.all" :count="counts.bad"/>
+      <OrganizationSectionsFeedbackLine label="Ужасно" :all="counts.all" :count="counts.terrible"/>
     </ul>
   </div>
 </template>
