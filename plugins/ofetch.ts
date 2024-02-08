3d8523b74088da2c22cd946c22d2ffd40bdae005
yuga <dima.rolick22@gmail.com>
2024-02-08 14:03:21 +0300

    add: Basic auth

--- plugins/ofetch.ts
+++ plugins/ofetch.ts
@@ -3,10 +3,16 @@ import {ofetch} from "ofetch";
 export default defineNuxtPlugin((nuxtApp) => {
     const config = useRuntimeConfig()
 
+    const username = 'myuser';
+    const password = 'mypass';
+
+    const encodedCredentials = btoa(`${username}:${password}`);
+
     const instance = ofetch.create({
         baseURL: config.public.baseUrl ?? "http://localhost:8080",
         headers: {
-            Accept: 'application/json'
+            Accept: 'application/json',
+            Authorization: `Basic ${encodedCredentials}`,
         }
     })
 
