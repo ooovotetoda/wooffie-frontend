d6806ac149e55445777ff90f06ea5654323bd1eb
yuga <dima.rolick22@gmail.com>
2024-02-19 13:15:20 +0300

    update: speed up loader

--- components/Loader.vue
+++ components/Loader.vue
@@ -14,7 +14,7 @@
   border-bottom-color: rgba(0, 0, 0, 0.1);
   width: 50px;
   height: 50px;
-  animation: spin 1s cubic-bezier(.6,.27,.4,.69) infinite;
+  animation: spin 0.5s cubic-bezier(.6,.27,.4,.69) infinite;
 }
 
 @keyframes spin {
