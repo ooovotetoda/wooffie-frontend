a271c592ee68031605676d11802c658adffb17c0
yuga <dima.rolick22@gmail.com>
2024-02-21 22:14:45 +0300

    fix: card text box

--- components/catalog/Card.vue
+++ components/catalog/Card.vue
@@ -258,7 +258,7 @@ const toggleIsActive = async () => {
   &__copy {
     display: -webkit-box;
     max-width: 100%;
-    -webkit-line-clamp: 6;
+    -webkit-line-clamp: 4;
     -webkit-box-orient: vertical;
     overflow: hidden;
     color: var(--primary-text-87, rgba(0, 0, 0, 0.87));
