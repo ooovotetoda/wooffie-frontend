162c3ea7c010726c920f5c0522969cb71e146b1d
Dmitry <dima.rolick22@gmail.com>
2024-05-02 13:52:58 +0300

    fix(CEO): yandex metrika

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,30 @@
 export default defineNuxtConfig({
   app: {
     head: {
+      script: [
+        {
+          innerHTML: `
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(96702656, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+            });
+          `,
+          type: 'text/javascript'
+        }
+      ],
+      noscript: [
+        {
+          innerHTML: '<div><img src="https://mc.yandex.ru/watch/96702656" style="position:absolute; left:-9999px;" alt="" /></div>'
+        }
+      ],
       title: 'Wooffie: Все Услуги для Животных на Одном Сайте',
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -44,14 +68,6 @@ export default defineNuxtConfig({
       }
     }
   },
-  //@ts-ignore
-  yandexMetrika: {
-    id: '96702656',
-    clickmap:true,
-    trackLinks:true,
-    accurateTrackBounce:true,
-    webvisor:true
-  },
   imports: {
     dirs: ['types/*.ts'],
   },
@@ -59,7 +75,6 @@ export default defineNuxtConfig({
     '@pinia/nuxt',
     '@vueuse/nuxt',
     '@nuxt/image',
-    'yandex-metrika-module-nuxt3',
     'vue3-carousel-nuxt',
     '@nuxt/ui',
   ],
