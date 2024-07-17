719757a70d598ba6a69ee4d6b42821b0830c566d
Dmitry <47058524+ooovotetoda@users.noreply.github.com>
2024-07-17 15:27:56 +0300

    feat: add useSignUp composable

---
+++ composables/useSignUp.ts
@@ -0,0 +1,43 @@
+import sendOTP from "~/utils/sendOTP";
+
+export function useSignUp() {
+  const authStorage = useSessionStorage('auth-store', { phone: '+79999999999', password: '', timer: '00' })
+
+  const phone = ref < string | null > (null)
+  const isPhoneValid = ref < boolean > (true)
+  const password = ref < string | null > (null)
+  const isPasswordValid = ref < boolean > (true)
+
+  async function handleSubmit() {
+    if (phone.value === null
+      || password.value === null
+      || password.value.length < 8
+      || !isPhoneValid.value
+      || !isPasswordValid.value) {
+      return
+    }
+
+    if (phone.value.startsWith('8')) {
+      phone.value = phone.value.replace('8', '+7')
+    }
+
+    authStorage.value.phone = phone.value
+    authStorage.value.password = password.value
+
+    const status = await sendOTP(phone.value)
+
+    if (status === 'OK') {
+      await navigateTo({
+        path: '/auth/code',
+        query: {
+          type: 'register',
+        },
+      })
+    }
+    else {
+      console.warn('failed to send OTP code')
+    }
+  }
+
+  return { phone, isPhoneValid, password, isPasswordValid, handleSubmit }
+}
--- pages/auth/signup.vue
+++ pages/auth/signup.vue
@@ -1,47 +1,9 @@
 <script setup lang="ts">
-import sendOTP from '~/utils/sendOTP'
-
 definePageMeta({
   layout: 'authorization',
 })
 
-const authStorage = useSessionStorage('auth-store', { phone: '+79999999999', password: '', timer: '00' })
-
-const phone = ref < string | null > (null)
-const isPhoneValid = ref < boolean > (true)
-const password = ref < string | null > (null)
-const isPasswordValid = ref < boolean > (true)
-
-async function handleSubmit() {
-  if (phone.value === null
-    || password.value === null
-    || password.value.length < 8
-    || !isPhoneValid.value
-    || !isPasswordValid.value) {
-    return
-  }
-
-  if (phone.value.startsWith('8')) {
-    phone.value = phone.value.replace('8', '+7')
-  }
-
-  authStorage.value.phone = phone.value
-  authStorage.value.password = password.value
-
-  const status = await sendOTP(phone.value)
-
-  if (status === 'OK') {
-    await navigateTo({
-      path: '/auth/code',
-      query: {
-        type: 'register',
-      },
-    })
-  }
-  else {
-    console.warn('failed to send OTP code')
-  }
-}
+const { phone, isPhoneValid, password, isPasswordValid, handleSubmit } = useSignUp()
 </script>
 
 <template>
