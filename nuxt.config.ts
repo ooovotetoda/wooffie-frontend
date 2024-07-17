ebaf2b8ba1d175727aee9066428ad47fcbadd685
Dmitry <47058524+ooovotetoda@users.noreply.github.com>
2024-07-17 13:35:23 +0300

    feat: delete Yandex Metrica

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -65,30 +65,6 @@ export default defineNuxtConfig({
   ],
   app: {
     head: {
-      script: [
-        {
-          innerHTML: `
-            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();
-            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-            ym(96702656, "init", {
-                clickmap:true,
-                trackLinks:true,
-                accurateTrackBounce:true,
-                webvisor:true
-            });
-          `,
-          type: 'text/javascript',
-        },
-      ],
-      noscript: [
-        {
-          innerHTML: '<div><img src="https://mc.yandex.ru/watch/96702656" style="position:absolute; left:-9999px;" alt="" /></div>',
-        },
-      ],
       title: 'Wooffie: Все Услуги для Животных на Одном Сайте',
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
